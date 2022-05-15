4eded191257b80afe055b6dd3bd5504d1a805962
Zygaa <patrykzygmunt10@gmail.com>
2022-05-15 10:47:55 +0200

    hi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,20 @@ class UserController extends Controller
       //return $reg->input();
       //return $reg->method();
       //return $reg->input('email');
-      $reg->validate([
-        'email' => 'required | min:5',
+      //$this->validate($reg, ['address' => 'required'], ['address.required' => 'Adres nie może być pusty']);
+
+      $reg->validate(
+        [
+        'email' => 'required | min:5 | email',
         'password' => 'required'
-      ]);
+        ],
+        [
+          'email.required' => 'Pole email jest wymagane',
+          'email.min' => 'Pole email musi mieć minimum 5 znaków',
+          'email.email' => 'Pole email musi być adresem poczty elektronicznej',
+          'password.required' => 'Pole hasło jest wymagane'
+        ]
+      );
       return $reg->input();
     }
 }
--- resources/views/userform.blade.php
+++ resources/views/userform.blade.php
@@ -9,7 +9,20 @@
 @endif
 <form action ="usercontroller" method="post">
   @csrf
-  <input type="text" name="email"><br><br>
-  <input type="password" name="password"><br><br>
+  <input type="text" name="imie" placeholder="Imię"><br><br>
+  <input type="email" name="email" placeholder="Adres email">
+  @error('email')
+    {{ $message }}
+  @enderror
+  <br><br>
+  <input type="password" name="password" placeholder="Hasło">
+  @error('password')
+    {{ $message }}
+  @enderror
+  <br><br>
+  <input type="radio" name="plec" id="mezczyzna">
+  <label for="mezczyzna">Mężczyzna</label>
+  <input type="radio" name="plec" id="kobieta">
+  <label for="kobieta">Kobieta</label><br><br>
   <input type="submit" value="Zatwierdź">
 </form>
